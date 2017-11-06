a567f0e17af18100d8bf0a37084495a6acbf2ec1
Alexey Svetliakov <asvetliakov@teledini.com>
2017-11-06 03:38:53 +0100

    feat: allow to pass custom class name replacer to serialize() (#23)

--- src/replace-class-names.js
+++ src/replace-class-names.js
@@ -1,15 +1,29 @@
-const replaceClassNames = (selectors, styles, code) => {
+function defaultClassNameReplacer(className, index) {
+  return `glamor-${index}`
+}
+
+const replaceClassNames = (
+  selectors,
+  styles,
+  code,
+  replacer = defaultClassNameReplacer,
+) => {
   let index = 0
-  return selectors.reduce((acc, className) => {
-    if (className.indexOf('.css-') === 0) {
-      const escapedRegex = new RegExp(
-        className.replace('.', '').replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'),
-        'g',
-      )
-      return acc.replace(escapedRegex, `glamor-${index++}`)
-    }
-    return acc
-  }, `${styles}\n\n${code}`)
+  return selectors.reduce(
+    (acc, className) => {
+      if (className.indexOf('.css-') === 0) {
+        const escapedRegex = new RegExp(
+          className
+            .replace('.', '')
+            .replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'),
+          'g',
+        )
+        return acc.replace(escapedRegex, replacer(className, index++))
+      }
+      return acc
+    },
+    `${styles}\n\n${code}`,
+  )
 }
 
 module.exports = {replaceClassNames}
